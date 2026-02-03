2dfdd17023e5eb6e592077e482928fc2095f65c6
dariaabylaj-beep <dariaabylaj@gmail.com>
2026-02-03 13:20:57 +0500

    Update InvalidInputException.java

--- src/exception/InvalidInputException.java
+++ src/exception/InvalidInputException.java
@@ -1,6 +1,6 @@
 package exception;
 
-public class InvalidInputException extends Exception {
+public class InvalidInputException extends Exception{
     public InvalidInputException(String message) {
         super(message);
     }
